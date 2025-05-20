447cc11da9ce52cd759ba2418ce23d0652160b72
Laurablem <eaa24lbvi@students.eaaa.dk>
2025-05-20 03:12:09 +0200

    tabel

--- js/proveegg.js
+++ js/proveegg.js
@@ -12,21 +12,3 @@ function toggleAlleNiveauer() {
   alleErAktive = !alleErAktive;
 }
 
-.bue-overskrift {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    width: 300px;
-    margin: 0 auto 30px;
-    transform: translateY(10px);
-    font-family: 'Super Funky', sans-serif;
-  }
-  
-  .bue-overskrift span {
-    display: inline-block;
-    font-size: 22px;
-    color: #f7f797;
-    transform: rotate(calc(var(--i) * 7deg - 40deg));
-    transform-origin: bottom center;
-  }
-  
